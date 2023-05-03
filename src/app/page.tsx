ea150f1d87ca1af195d2fe2d2cc0c802ef26ec9c
Willem-Jaap <67187467+Willem-Jaap@users.noreply.github.com>
2023-05-03 11:35:44 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Component from '~components/Component';
+
+const Page = () => {
+    return (
+        <>
+            <h1 className="m-2">Next Foundation</h1>
+            <Component />
+        </>
+    );
+};
+
+export default Page;
