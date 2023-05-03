f292189762d4a1070af5623b56d4306693158970
Willem-Jaap <willemjaapk@gmail.com>
2023-05-03 12:01:36 +0200

    feat: add initial content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
-import Component from '~components/Component';
+import Link from 'next/link';
 
 const Page = () => {
     return (
-        <>
-            <h1 className="m-2">Next Foundation</h1>
-            <Component />
-        </>
+        <div className="page">
+            <div className="page__content">
+                <h1 className="m-y-1">Hey, I&apos;m Willem-Jaap</h1>
+                <p className="t--gray">A Software Developer based in The Netherlands.</p>
+                <p className="t--gray">
+                    I&apos;m strive to create software that is fast, easy to use, maintainable and
+                    scalable.
+                    <br />
+                    I&apos;m a big fan of Next.js, Typescript, SCSS & Laravel.
+                </p>
+                <p className="t--gray">
+                    I&apos;m currently working at{' '}
+                    <Link href="https://pixelperfect.agency">Pixel Perfect Agency</Link>.
+                </p>
+                <p className="t--gray">
+                    You can find me on <Link href="https://github.com/willem-jaap">Github</Link>.
+                </p>
+            </div>
+        </div>
     );
 };
 
