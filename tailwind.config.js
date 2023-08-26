4655b9ccf67a2e8053f66cea5657bf0724680ea6
Willem-Jaap <willemjaapk@gmail.com>
2023-08-26 16:53:42 +0200

    feat: add charade color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                charade: {
+                    default: '#2D2C35',
+                    50: '#E7E6EA',
+                    100: '#D1CFD8',
+                    200: '#A5A2B3',
+                    300: '#7A768F',
+                    400: '#545062',
+                    500: '#2D2C35',
+                    600: '#24222A',
+                    700: '#1A191F',
+                    800: '#111014',
+                    900: '#070708',
+                    950: '#020203',
+                },
+            },
+        },
     },
     plugins: [],
 };
