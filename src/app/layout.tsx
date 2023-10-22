293b159ebf16103c6c6f8b6b0b4db69bde8d5c89
Willem-Jaap <willemjaapk@gmail.com>
2023-10-22 13:24:00 +0200

    fix(navigation): fix z-index and max-width issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const RootLayout = ({ children }: PropsWithChildren) => {
     return (
         <html className={cn(InterFont.className, 'scroll-smooth')}>
             <body className="relative max-w-[100vw] min-h-screen bg-charade-900 text-charade-50 overflow-x-hidden">
-                <div className="absolute top-0 left-0 w-full z-10">
+                <div className="absolute top-0 left-0 w-full z-20">
                     <div className="flex gap-4 justify-between mx-8 py-10 border-b border-charade-700">
                         <div className="flex items-center gap-6">
                             <Logo />
