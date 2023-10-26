f758aebc5b412c53035c67871bbac7f26a0c2a31
Willem-Jaap <willemjaapk@gmail.com>
2023-10-26 16:33:19 +0200

    refactor: use state for active state, handle touch toggle

--- src/components/misc/image-with-content-overlay.tsx
+++ src/components/misc/image-with-content-overlay.tsx
@@ -1,4 +1,6 @@
-import { type ReactNode, useRef } from 'react';
+'use client';
+
+import { type ReactNode, useEffect, useRef, useState } from 'react';
 
 import gsap from 'gsap';
 import Image from 'next/image';
@@ -14,32 +16,33 @@ interface Props {
 
 const ImageWithContentOverlay = ({ children, src, alt, className }: Props) => {
     const overlayRef = useRef<HTMLDivElement>(null);
-    const setActive = () => {
-        gsap.to(overlayRef.current, {
-            duration: 0.5,
-            opacity: 1,
-            ease: 'power3.out',
-        });
-    };
+    const [active, setActive] = useState(false);
 
-    const setInactive = () => {
+    useEffect(() => {
+        if (active) {
+            gsap.to(overlayRef.current, {
+                duration: 0.5,
+                opacity: 1,
+                ease: 'power3.out',
+            });
+            return;
+        }
         gsap.to(overlayRef.current, {
             duration: 0.5,
             opacity: 0,
             ease: 'power3.out',
         });
-    };
+    }, [active]);
+
     return (
         <div
             className={cn(
                 'relative overflow-hidden rounded-2xl h-[40vh] md:h-[60vh] min-h-[20rem] border border-charade-800',
                 className,
             )}
-            onMouseEnter={setActive}
-            onMouseLeave={setInactive}
-            onTouchStart={setActive}
-            onTouchEnd={setInactive}
-            onTouchCancel={setInactive}>
+            onMouseEnter={() => setActive(true)}
+            onMouseLeave={() => setActive(false)}
+            onTouchStart={() => setActive(!active)}>
             <Image src={src} alt={alt} fill className="object-cover" />
             <div
                 className="absolute inset-0 p-4 grid place-items-center items-center bg-charade-900/75 opacity-0"
