16b81694df9741e0a6960feccd40672ad04a8a32
Willem-Jaap <willemjaapk@gmail.com>
2023-10-26 15:36:59 +0200

    feat: add randomization on scroll to `PaddedWithRandomized` function

--- src/components/misc/padded-with-randomized.tsx
+++ src/components/misc/padded-with-randomized.tsx
@@ -24,6 +24,7 @@ const PaddedWithRandomized = ({ text }: Props) => {
     const textRef = useRef<HTMLSpanElement>(null);
     const secondLineRef = useRef<HTMLDivElement>(null);
     const thirdLineRef = useRef<HTMLDivElement>(null);
+    const hasAnimated = useRef(false);
 
     const randomizeLines = () => {
         if (firstLineRef.current && secondLineRef.current && thirdLineRef.current) {
@@ -33,9 +34,26 @@ const PaddedWithRandomized = ({ text }: Props) => {
         }
     };
 
+    const onScroll = () => {
+        let previousScroll = 0;
+
+        if (window.scrollY != previousScroll) {
+            randomizeLines();
+        }
+
+        previousScroll = Math.round(window.scrollY / 32);
+    };
+
     useEffect(() => {
         const characters = text.split('');
         let index = 0;
+
+        if (hasAnimated.current) return;
+
+        hasAnimated.current = true;
+        firstLineRef.current!.innerText = getRandomCharacters();
+        secondLineRef.current!.innerText = getRandomCharacters(100 - text.length);
+        thirdLineRef.current!.innerText = getRandomCharacters();
 
         const interval = setInterval(() => {
             if (!textRef.current) return;
@@ -50,22 +68,18 @@ const PaddedWithRandomized = ({ text }: Props) => {
             // Stop the interval when all letters have been added
             if (index >= characters.length) clearInterval(interval);
         }, 75);
-    }, [text]);
+
+        window.addEventListener('scroll', onScroll);
+    }, []);
 
     return (
         <div className="text-charade-700 text-3xl">
-            <div className="break-all overflow-hidden h-[1.5em]" ref={firstLineRef}>
-                {getRandomCharacters()}
-            </div>
+            <div className="break-all overflow-hidden h-[1.5em]" ref={firstLineRef} />
             <div className="break-all overflow-hidden h-[1.5em]">
-                <span className="text-charade-100 break-all h-[1.5em]" ref={textRef}>
-                    {getRandomCharacters(text.length)}
-                </span>
-                <span ref={secondLineRef}>{getRandomCharacters(100 - text.length)}</span>
+                <span className="text-charade-100 break-all h-[1.5em]" ref={textRef} />
+                <span ref={secondLineRef} />
             </div>
-            <div className="break-all overflow-hidden h-[1.5em]" ref={thirdLineRef}>
-                {getRandomCharacters()}
-            </div>
+            <div className="break-all overflow-hidden h-[1.5em]" ref={thirdLineRef} />
         </div>
     );
 };
