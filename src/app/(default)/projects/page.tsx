34f37f306780d13038e5dd4fb768af350d37019e
Willem-Jaap <willemjaapk@gmail.com>
2023-11-02 14:40:37 +0100

    feat(projects): add `PaddedWithRandomized` component to projects page, update spacing

--- src/app/(default)/projects/page.tsx
+++ src/app/(default)/projects/page.tsx
@@ -1,17 +1,16 @@
+import PaddedWithRandomized from '~components/misc/padded-with-randomized';
 import ProjectsGrid from '~projects/components/projects-grid';
 
 const Page = () => {
     return (
         <>
-            <div className="max-w-5xl mx-auto px-8 py-56">
-                <div className="border-b border-b-charade-800 mb-12">
-                    <div className="flex justify-between items-center gap-4 mb-4">
-                        <h2 className="text-2xl leading-tight">Projects</h2>
-                    </div>
-                    <p className="text-charade-400 pb-6">
+            <div className="mx-column-1  py-56">
+                <div className="border-b border-b-charade-800 mb-24">
+                    <PaddedWithRandomized text="Projects" />
+                    <p className="text-charade-400 my-8 max-w-5xl">
                         I love building things. Here are some of my projects. Some are open source,
-                        and you can find the code on GitHub. I clarify some of my design decisions
-                        in the the post for each project which might be interesting to read.
+                        you can find the code on GitHub. I clarify some of my design decisions in
+                        the the post for each project which might be interesting to read.
                     </p>
                 </div>
                 <ProjectsGrid />
