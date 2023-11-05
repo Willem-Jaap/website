ef90807718a7b1515f2cab87ad9d34da7412a00d
Willem-Jaap <willemjaapk@gmail.com>
2023-11-05 14:39:35 +0100

    fix: remove unused shadcn config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ['class'],
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {
         fontSize: {
@@ -32,39 +31,6 @@ module.exports = {
                     800: '#18171C',
                     900: '#111014',
                     950: '#0D0D0F',
-                },
-                border: 'hsl(var(--border))',
-                input: 'hsl(var(--input))',
-                ring: 'hsl(var(--ring))',
-                background: 'hsl(var(--background))',
-                foreground: 'hsl(var(--foreground))',
-                primary: {
-                    DEFAULT: 'hsl(var(--primary))',
-                    foreground: 'hsl(var(--primary-foreground))',
-                },
-                secondary: {
-                    DEFAULT: 'hsl(var(--secondary))',
-                    foreground: 'hsl(var(--secondary-foreground))',
-                },
-                destructive: {
-                    DEFAULT: 'hsl(var(--destructive))',
-                    foreground: 'hsl(var(--destructive-foreground))',
-                },
-                muted: {
-                    DEFAULT: 'hsl(var(--muted))',
-                    foreground: 'hsl(var(--muted-foreground))',
-                },
-                accent: {
-                    DEFAULT: 'hsl(var(--accent))',
-                    foreground: 'hsl(var(--accent-foreground))',
-                },
-                popover: {
-                    DEFAULT: 'hsl(var(--popover))',
-                    foreground: 'hsl(var(--popover-foreground))',
-                },
-                card: {
-                    DEFAULT: 'hsl(var(--card))',
-                    foreground: 'hsl(var(--card-foreground))',
                 },
             },
             padding: {
