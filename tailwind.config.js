e339b87b4ab220cf7481f433b76ed795d81a778a
Willem-Jaap <willemjaapk@gmail.com>
2023-11-05 14:41:56 +0100

    feat: load `@tailwindcss/typography` plugin in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -114,5 +114,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require('tailwindcss-animate')],
+    plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
 };
