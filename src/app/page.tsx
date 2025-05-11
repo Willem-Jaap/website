2921c773ea5aefef008402c0a51d21c4fe8da0fa
Willem-Jaap <willemjaapk@gmail.com>
2025-05-11 14:17:17 +0200

    feat: remove component, add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Component from '~components/component';
+import Link from 'next/link';
 
 const Page = async () => {
     return (
         <div className="m-8">
-            <h1 className="font-semibold text-2xl">Next Foundation</h1>
-            <Component />
+            <h1 className="font-medium font-serif text-4xl leading-tight md:text-5xl lg:text-6xl">
+                Hi, I'm <span className="gradient-text">Willem-Jaap</span>
+                <br />
+                Software Developer
+                <br />
+                from the Netherlands
+            </h1>
+            <p className="max-w-md text-lg text-muted-foreground">
+                I build elegant, user-centric web experiences with a focus on performance and
+                accessibility.
+            </p>
         </div>
     );
 };
