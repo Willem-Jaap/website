508ad1da3e5d751bc87d4d067e6280e9dc6e79f2
Willem-Jaap <willemjaapk@gmail.com>
2025-05-11 14:07:58 +0200

    feat: add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,25 @@
 import type { Metadata } from 'next';
-import { Albert_Sans } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import type { PropsWithChildren } from 'react';
 
+import { Header } from '~components/header';
+import cn from '~utils/cn';
 import '~styles/global.css';
 
 import { env } from '~/env';
 
-const AlbertSansFont = Albert_Sans({
+const InterFont = Inter({
     subsets: ['latin'],
 });
 
 const RootLayout = ({ children }: PropsWithChildren) => {
     return (
-        <html className={AlbertSansFont.className} lang="en">
-            <body>{children}</body>
+        <html className={cn(InterFont.className, 'scroll-smooth antialiased')} lang="en">
+            <body className="relative min-h-screen max-w-screen overflow-x-hidden bg-neutral-50 text-base text-neutral-950">
+                <Header />
+                {children}
+                {/* <Analytics /> */}
+            </body>
         </html>
     );
 };
@@ -21,15 +27,33 @@ const RootLayout = ({ children }: PropsWithChildren) => {
 export const metadata: Metadata = {
     metadataBase: new URL(env.NEXT_PUBLIC_URL as string),
     title: {
-        template: '%s - Next Foundation',
-        default: 'Next Foundation',
+        default: 'Willem-Jaap - Portfolio, insights and more',
+        template: '%s | Willem-Jaap',
     },
-    description: 'A very solid foundation for scalable Next.js projects.',
+    description:
+        'Software developer, proficient in Next.js, Typescript, Tailwind, Laravel and more. Read about my latest insights and projects.',
+    keywords: [
+        'willem-jaap',
+        'developer',
+        'software',
+        'portfolio',
+        'insights',
+        'projects',
+        'next',
+        'next.js',
+        'typescript',
+        'tailwind',
+        'laravel',
+        'react',
+    ],
     openGraph: {
-        url: new URL(env.NEXT_PUBLIC_URL as string),
-        title: 'Next Foundation - A very solid foundation for scalable Next.js projects',
-        siteName: 'Next Foundation',
+        title: 'Willem-Jaap - Portfolio, insight and more',
+        description:
+            'Software developer, proficient in Next.js, Typescript, Tailwind, Laravel and more. Read about my latest insights and projects.',
+        type: 'website',
         locale: 'en_US',
+        url: new URL(env.NEXT_PUBLIC_URL as string),
+        siteName: 'Willem-Jaap Portfolio',
     },
 };
 
---
+++ src/modules/core/components/header.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import cn from '~utils/cn';
+
+const items = [
+    { label: 'Home', href: '/' },
+    { label: 'Insights', href: '/insights' },
+    { label: 'Projects', href: '/projects' },
+    { label: 'About', href: '/about' },
+];
+
+const Header = () => {
+    const pathname = usePathname();
+    return (
+        <header className="sticky top-0 z-50 flex justify-between border-neutral-200 border-b bg-neutral-50/80 px-4 py-6 backdrop-blur-md md:px-6">
+            <Link href="/" className="font-medium font-serif text-2xl">
+                WJ<span className="text-blue-500">.</span>
+            </Link>
+            <nav className="hidden items-center gap-6 md:flex">
+                {items.map(item => (
+                    <Link
+                        key={item.href}
+                        href={item.href}
+                        className={cn(
+                            'relative text-base tracking-tight transition-colors hover:text-dutch-blue',
+                            pathname === item.href
+                                ? 'font-medium text-blue-500'
+                                : 'text-neutral-800/80',
+                        )}
+                    >
+                        {item.label}
+                    </Link>
+                ))}
+            </nav>
+        </header>
+    );
+};
+
+export { Header };
